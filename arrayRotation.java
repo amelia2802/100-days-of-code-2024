e0208c4dfa15ef14eef9a5c71bd329a41f37251c
amelia2802 <amelia280111dutta@gmail.com>
2024-01-22 16:32:59 +0530

    initialization

---
+++ arrayRotation.java
@@ -0,0 +1,12 @@
+/*  
+    Array Rotation
+    New code challenge! This is a great way to practice your coding skills and get feedback from the community. We will post a new challenge roughly every 2 days. Good luck!
+    Description
+    Write a function called 'rotateArray' that takes an array and an integer as arguments. The function should rotate the array to the right by the integer amount, and return the resulting array.
+    Rules
+    You must post your code in the code challenge channel
+    The array can contain any type of elements.
+    The integer will always be positive and less than or equal to the length of the array.
+    Example
+    rotateArray([1, 2, 3, 4, 5], 2) => [4, 5, 1, 2, 3]
+*/
