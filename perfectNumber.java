abeccb85894a43d9686dbc900d171c66b3ccc238
amelia2802 <amelia280111dutta@gmail.com>
2024-01-26 22:12:27 +0530

    perfect number

---
+++ perfectNumber.java
@@ -0,0 +1,40 @@
+/*
+ *  Perfect Number Check
+    New code challenge! This is a great way to practice your coding skills and get feedback from the community. We will post a new challenge roughly every 2 days. Good luck!
+    Description
+    A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. Your task is to create a function that checks if a given number is perfect.
+    Rules
+    You must post your code in the code challenge channel
+    The function should return true if the number is perfect and false if it is not.
+    The number will be a positive integer.
+    Example
+    perfectNumberCheck(6) => true
+ */
+import java.util.*;
+
+public class perfectNumber {
+    public static boolean perfectNumberCheck(int num){
+        int sumFact = 0;
+
+        if(num<0){
+            return false;
+        }
+        
+        for(int i=1;i<=num/2;i++){
+            if(num%i == 0){
+                sumFact+=i;
+            }
+        }
+
+        return (num == sumFact);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to check:");
+
+        int inputNumber = sc.nextInt();
+        System.out.println(perfectNumberCheck(inputNumber));
+
+        sc.close();
+    }
+}
